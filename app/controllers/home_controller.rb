d5f89ba1c9f9ecbe91a5aaff8fa9e8039316a5f4
Courtney <courtneymkk@gmail.com>
2018-04-25 18:31:31 -0700

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,38 @@
+class HomeController < ApplicationController
+  def index
+    color = "%06x" % (rand * 0xffffff)
+    color_two = "%06x" % (rand * 0xffffff)
+    @color = "#"+color
+    @color_two = "#"+color_two
+  end
+  def place
+    lat = request.location.latitude
+    lng = request.location.longitude
+    radius = 500
+    type = params[:type] 
+    base = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?"
+    key = "key=AIzaSyCCDOMuoRJs6evSFsAs0JXrWY4Mt3mZCYk"
+    location = "&location=#{lat},#{lng}"
+    radius = "&radius=#{radius}"
+    type = "&types=#{type}"
+    url = base + key + location + radius + type 
+    session[:url] = url
+    response = HTTP.get(url)
+    parsed_response = JSON.parse(response.body)
+    length = parsed_response['results'].length
+    number = rand(0..length)
+    random = number
+    session[:place] = parsed_response['results'][random]['name']
+    session[:address] = parsed_response['results'][random]['vicinity']
+    redirect_to "/random"
+  end
+  def random
+    color = "%06x" % (rand * 0xffffff)
+    color_two = "%06x" % (rand * 0xffffff)
+    @color = "#"+color
+    @color_two = "#"+color_two
+    @url = session[:url]
+    @place = session[:place]
+    @address = session[:address]
+  end 
+end
